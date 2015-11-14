37125086e0711480fb93312ff3fb40b397aa1909
lceberg <kaloyan.github@gmail.com>
2015-11-14 13:11:24 +0200

    Added blank index.php files

--- vuebox.php
+++ vuebox.php
@@ -1,1 +1,10 @@
 <?php
+/*
+| Library Name: VueBox
+| Library URI: https://github.com/lceberg/VueBox
+| Description: A WordPress library for adding custom fields and menu pages
+| Version: 1.0.0 (major.minor.bugfix)
+| Author: Kaloyan Ivanov
+| Author URI: https://github.com/lceberg
+*/
+
