5355329e22944e20d639f1c42469b4e0ace19cd3
lceberg <kaloyan.github@gmail.com>
2015-11-16 19:58:28 +0200

    Added textarea field

--- assets/js/vuebox-fields.js
+++ assets/js/vuebox-fields.js
@@ -13,4 +13,17 @@
 		props: ['title', 'name', 'value']
 	} );
 
+	/* Textarea */
+	Vue.component( 'vuebox-textarea', {
+		template: '' +
+		'<div class="vuebox-field vuebox-field-text">' +
+			'<div class="vuebox-field-title">' +
+				'<label>{{ title }}</label>' +
+			'</div>' +
+			'<textarea class="widefat" name="{{ name }}" type="text">{{ value }}</textarea>' +
+		'</div>',
+
+		props: ['title', 'name', 'value']
+	} );
+
 })();
