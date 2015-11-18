518a1e424bd3c94e0c5ac537c7f4a10f0879a7ba
lceberg <kaloyan.github@gmail.com>
2015-11-18 20:33:09 +0200

    Added meta/option retrieving function

--- functions/helpers.php
+++ functions/helpers.php
@@ -1,11 +1,23 @@
 <?php
 
-function vuebox_path( $file ) {
-	$file_path  = str_replace( STYLESHEETPATH, '', dirname( $file ) );
+function vuebox( $field, $post_id = false ) {
+	$value = '';
 
-	return VUEBOX_THEME_PATH . $file_path;
+	if ( $post_id ) {
+		$value = get_post_meta( $post_id, "_{$field}", true );
+	} else {
+		$value = get_option( $field );
+	}
+
+	return $value;
 }
 
 function vuebox_generate_title( $name ) {
 	return ucwords( str_replace( '_', ' ', $name ) );
+}
+
+function vuebox_path( $file ) {
+	$file_path  = str_replace( STYLESHEETPATH, '', dirname( $file ) );
+
+	return VUEBOX_THEME_PATH . $file_path;
 }
