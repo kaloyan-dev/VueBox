398083bc6b38a0c84ebb9c09a53f668eb4c901b1
lceberg <kaloyan.github@gmail.com>
2015-11-21 23:24:15 +0200

    Added reusable field rendering function

--- class/VueBoxContainerBox.php
+++ class/VueBoxContainerBox.php
@@ -54,63 +54,7 @@ class VueBoxContainerBox {
 	public function render_meta_box() {
 		$container_id = str_replace( '_', '-', $this->data['name'] ); ?>
 		<div id="vuebox-<?php echo $container_id; ?>" class="vuebox-container">
-			<?php
-				foreach ( $this->data['fields'] as $field ):
-					$type           = $field->data['type'];
-					$name           = $field->data['name'];
-					$title          = $field->data['title'];
-					$subfields_data = '';
-
-					if ( $type !== 'repeater' ) {
-						$title .= ':';
-					} else {
-						$subfields      = $field->data['fields'];
-						$subfields_data = json_encode( $field->data['fields'] );
-					}
-
-					$value = '';
-
-					if ( empty ( $_GET['post'] ) ) {
-						break;
-					}
-
-					$value = get_post_meta( $_GET['post'], "_{$name}", true );
-
-					if ( $type !== 'repeater' ): ?>					
-						<vuebox-<?php echo $type; ?>
-							title="<?php echo $title; ?>"
-							name="<?php echo $name; ?>"
-							value="<?php echo $value; ?>">
-						</vuebox-<?php echo $type; ?>>
-					<?php else: ?>
-						<vuebox-<?php echo $type; ?>
-							title="<?php echo $title; ?>"
-							name="<?php echo $name; ?>"
-							subfields="<?php echo $subfields_data; ?>">
-
-							<div class="vuebox-repeater-fieldwrap">
-								<?php
-									$subfield_index = 0;
-
-									foreach ( $subfields as $subfield ):
-										$subfield_type  = $subfield->data['type'];
-										$subfield_title = $subfield->data['title'];
-										$subfield_name  = $name . '[' . $subfield_index . ']' . $subfield->data['name'];
-										$subfield_value = get_post_meta( $_GET['post'], "_{$name}[{$subfield_index}]{$subfield_name}", true ); ?>
-										<vuebox-<?php echo $subfield_type; ?>
-											title="<?php echo $subfield_title; ?>"
-											name="<?php echo $subfield_name; ?>"
-											value="<?php echo $subfield_value; ?>">
-										</vuebox-<?php echo $subfield_type; ?>>
-
-										<?php
-									endforeach;
-								?>
-							</div>
-						</vuebox-<?php echo $type; ?>>
-					<?php endif;
-				endforeach;
-			?>
+			<?php vuebox_render_fields( $this, $this->data['fields'], true ); ?>
 		</div>
 		<?php
 	}
@@ -122,17 +66,7 @@ class VueBoxContainerBox {
 				<h2><?php echo $this->data['title']; ?></h2>
 
 				<form action="<?php echo add_query_arg( 'page', 'vuebox-' . sanitize_title( $this->data['title'] ) ); ?>" method="post">
-					<?php
-						foreach ( $this->data['fields'] as $field ):
-							$type  = $field['type'];
-							$name  = $field['name'];
-							$title = $field['title'] . ':';
-							$value = get_option( $name );
-							?>
-							
-							<vuebox-<?php echo $type; ?> title="<?php echo $title; ?>" name="<?php echo $name; ?>" value="<?php echo $value; ?>"></vuebox-<?php echo $type; ?>>				
-						<?php endforeach;
-					?>
+					<?php vuebox_render_fields( $this, $this->data['fields'] ); ?>
 					<div class="vuebox-form-actions">
 						<input type="submit" class="button button-primary" value="<?php _e( 'Save Options', 'vuebox' ); ?>" />
 					</div>
