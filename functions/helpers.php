428d8b59f1e7316df8ce5251f1a619ebbbd4e774
lceberg <kaloyan.github@gmail.com>
2015-11-22 16:53:59 +0200

    Added potential support for nested repeaters

--- functions/helpers.php
+++ functions/helpers.php
@@ -22,64 +22,64 @@ function vuebox_path( $file ) {
 	return VUEBOX_THEME_PATH . $file_path;
 }
 
+function vuebox_render_field( $field, $post_fields = false, $parent_name = false, $subfield_index = 0 ) {
+	$type           = $field->data['type'];
+	$name           = $field->data['name'];
+	$title          = $field->data['title'];
+	$subfields      = array();
+	$subfields_data = '';
+
+	if ( $parent_name ) {
+		$name  = $parent_name . '[' . $subfield_index . ']' . $name;
+		$value = get_post_meta( $_GET['post'], "_{$parent_name}[{$subfield_index}]{$name}", true );
+	}
+
+	if ( $type !== 'repeater' ) {
+		$title .= ':';
+	} else {
+		$subfields      = $field->data['fields'];
+		$subfields_data = json_encode( $field->data['fields'] );
+	}
+
+	$value = '';
+
+	if ( $post_fields ) {
+		if ( empty ( $_GET['post'] ) ) {
+			break;
+		}
+
+		$value = get_post_meta( $_GET['post'], "_{$name}", true );
+	} else {
+		$value = get_option( $name );			
+	}
+
+	if ( $type !== 'repeater' ): ?>					
+		<vuebox-<?php echo $type; ?>
+			title="<?php echo $title; ?>"
+			name="<?php echo $name; ?>"
+			value="<?php echo $value; ?>">
+		</vuebox-<?php echo $type; ?>>
+	<?php else: ?>
+		<vuebox-<?php echo $type; ?>
+			title="<?php echo $title; ?>"
+			name="<?php echo $name; ?>"
+			subfields="<?php echo $subfields_data; ?>">
+
+			<div class="vuebox-repeater-fieldwrap">
+				<?php
+					$subfield_index = 0;
+
+					foreach ( $subfields as $subfield ):
+						vuebox_render_field( $subfield, $post_fields, $name, $subfield_index );
+					endforeach;
+				?>
+			</div>
+		</vuebox-<?php echo $type; ?>>
+	<?php endif;
+}
+
 function vuebox_render_fields( $fields_class, $fields, $post_fields = false ) {
 	foreach ( $fields_class->data['fields'] as $field ):
-		$type           = $field->data['type'];
-		$name           = $field->data['name'];
-		$title          = $field->data['title'];
-		$subfields_data = '';
-
-		if ( $type !== 'repeater' ) {
-			$title .= ':';
-		} else {
-			$subfields      = $field->data['fields'];
-			$subfields_data = json_encode( $field->data['fields'] );
-		}
-
-		$value = '';
-
-		if ( $post_fields ) {
-			if ( empty ( $_GET['post'] ) ) {
-				break;
-			}
-
-			$value = get_post_meta( $_GET['post'], "_{$name}", true );
-		} else {
-			$value = get_option( $name );			
-		}
-
-		if ( $type !== 'repeater' ): ?>					
-			<vuebox-<?php echo $type; ?>
-				title="<?php echo $title; ?>"
-				name="<?php echo $name; ?>"
-				value="<?php echo $value; ?>">
-			</vuebox-<?php echo $type; ?>>
-		<?php else: ?>
-			<vuebox-<?php echo $type; ?>
-				title="<?php echo $title; ?>"
-				name="<?php echo $name; ?>"
-				subfields="<?php echo $subfields_data; ?>">
-
-				<div class="vuebox-repeater-fieldwrap">
-					<?php
-						$subfield_index = 0;
-
-						foreach ( $subfields as $subfield ):
-							$subfield_type  = $subfield->data['type'];
-							$subfield_title = $subfield->data['title'];
-							$subfield_name  = $name . '[' . $subfield_index . ']' . $subfield->data['name'];
-							$subfield_value = get_post_meta( $_GET['post'], "_{$name}[{$subfield_index}]{$subfield_name}", true ); ?>
-							<vuebox-<?php echo $subfield_type; ?>
-								title="<?php echo $subfield_title; ?>"
-								name="<?php echo $subfield_name; ?>"
-								value="<?php echo $subfield_value; ?>">
-							</vuebox-<?php echo $subfield_type; ?>>
-
-							<?php
-						endforeach;
-					?>
-				</div>
-			</vuebox-<?php echo $type; ?>>
-		<?php endif;
+		vuebox_render_field( $field, $post_fields );
 	endforeach;
 }
