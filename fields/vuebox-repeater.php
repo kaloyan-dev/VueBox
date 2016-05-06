d557042fd85809cad8dddeced1073a3588e2d659
Kaloyan Ivanov <kaloyanxivanov@gmail.com>
2016-05-06 20:10:07 +0300

    Improve repeater styles

--- fields/vuebox-repeater.php
+++ fields/vuebox-repeater.php
@@ -19,7 +19,7 @@
 			</div>
 		</div>
 		<div class="vuebox-repeater-actions">
-			<a href="#" @click.prevent="addFieldset"><?php _e( 'Add Fieldset', 'vuebox' ); ?></a>
+			<a href="#" @click.prevent="addFieldset"><?php _e( 'Add Entry', 'vuebox' ); ?></a>
 		</div>
 		<p v-if="caption" class="vuebox-caption">{{ caption }}</p>
 	</div>
