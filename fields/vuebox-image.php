ee2a87cedf75e2f92b3e166cb60b9f53be3e0b24
Kaloyan Ivanov <kaloyanxivanov@gmail.com>
2016-05-09 21:55:29 +0300

    Update image remove button styles

--- fields/vuebox-image.php
+++ fields/vuebox-image.php
@@ -6,9 +6,9 @@
 		<input name="{{ name }}" type="hidden" value="{{ value }}">
 		<div class="vuebox-image-preview" v-if="url">
 			<img :src="url" alt="" />			
+			<a class="vuebox-remove-image" href="#" @click.prevent="removeImage"><span class="fa fa-times"></span></a>
 		</div>
 		<input type="button" class="button button-primary" value="<?php _e( 'Select', 'vuebox' ); ?>" @click.prevent="pickImage" />
-		<input v-if="url" type="button" class="button" value="<?php _e( 'Remove', 'vuebox' ); ?>" @click.prevent="removeImage" />
 		<p v-if="caption" class="vuebox-caption">{{ caption }}</p>
 	</div>	
 </vuebox-image>
