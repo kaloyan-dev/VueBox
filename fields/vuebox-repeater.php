86b6244f007b77f2311bebe002bd34fc3b289b62
Kaloyan Ivanov <kaloyanxivanov@gmail.com>
2016-05-12 21:11:02 +0300

    Fix repeater initialization logic

--- fields/vuebox-repeater.php
+++ fields/vuebox-repeater.php
@@ -3,20 +3,16 @@
 		<input type="hidden" name="{{ name }}[]" />
 		<div class="vuebox-field-title">{{ title }}</div>
 		<div class="vuebox-repeater-children" v-sortable:fieldsets="{ animation: 200 }">
-			<div class="vuebox-repeater-fieldwrap" v-for="n in fieldsets">
-				<span class="vuebox-fieldwrap-handle"><i class="fa fa-arrows-v"></i></span>
-				<?php
-					if ( ! $fieldsets ) {
-						_e( 'No fields added.', 'vuebox' );
-					} else {
-						for ( $i = 0; $i < $fieldsets; $i++ ):
-							foreach ( $subfields as $subfield ):
-								vuebox_render_field( $subfield, $post_fields, $name, $i );
-							endforeach;
-						endfor;						
-					}
-				?>
-			</div>
+			<?php for ( $i = 0; $i < $fieldsets; $i++ ): ?>
+				<div class="vuebox-repeater-fieldwrap">
+					<span class="vuebox-fieldwrap-handle"><i class="fa fa-arrows-v"></i></span>
+					<?php
+						foreach ( $subfields as $subfield ):
+							vuebox_render_field( $subfield, $post_fields, $name, $i );
+						endforeach;
+					?>
+				</div>
+			<?php endfor; ?>
 		</div>
 		<div class="vuebox-repeater-actions">
 			<a href="#" @click.prevent="addFieldset"><?php _e( 'Add Entry', 'vuebox' ); ?></a>
