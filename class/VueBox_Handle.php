6971757eea404d510cac1dcb6217fef1b68865c4
Kaloyan Ivanov <kaloyanxivanov@gmail.com>
2016-05-16 20:34:19 +0300

    Add better repeater initialization

--- class/VueBox_Handle.php
+++ class/VueBox_Handle.php
@@ -1,6 +1,12 @@
 <?php
 
 class VueBox_Handle extends VueBox {
+
+	function __construct() {
+		$this->data['fields'] = array();
+		$this->data['labels'] = __( 'Add Entry', 'vuebox' );
+	}
+
 	public function add_fields( $fields ) {
 		if ( $this->data['type'] !== 'repeater' ) {
 			return $this;
@@ -9,5 +15,9 @@ class VueBox_Handle extends VueBox {
 		$this->data['fields'] = $fields;
 
 		return $this;
+	}
+
+	public function set_labels( $labels ) {
+		$this->data['labels'] = $labels;
 	}
 }
